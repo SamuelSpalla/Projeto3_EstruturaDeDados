bb4f3f934921a59c5edbeb6efa87419758145d80
YagoTarsin <102929131+YagoTarsin@users.noreply.github.com>
2023-03-27 16:09:59 -0300

    Update Projeto3.py

--- Projeto3.py
+++ Projeto3.py
@@ -3,27 +3,43 @@ import matplotlib.pyplot as plt
 
 
 def p1():
-    elementos = [['a', 'b', 'c', 'd'], ['q', 'i', 'n', 'm'], ['f', 'e', 'h', 'j'], ['p', 'o', 'l', 'g']]
+    elementos = [
+        ['a', 'b', 'c', 'd'],
+        ['q', 'i', 'n', 'm'],
+        ['f', 'e', 'h', 'j'],
+        ['p', 'o', 'l', 'g']
+    ]
     final = list()
     for x in range(len(elementos)):
-        for y in range(len(elementos[x])):
-            final.append(elementos[x][y])
+        final += elementos[x]
+    print(f'Crescente:   {sorted(final)}\n'
+          f'Decrescente: {sorted(final, reverse=True)}')
 
-    print(f'Crescente:   {sorted(final)}\nDecrescente: {sorted(final, reverse=True)}')
+
+def BigO():
+    ns = list()
+    tempos = list()
+    for c in range(1, 31):
+        inicio = time.perf_counter()
+        p1()
+        final_time = time.perf_counter()
+        ms = (final_time - inicio) * 10 ** 6
+        ns.append(c)
+        tempos.append(ms)
+
+    plt.plot(ns, tempos)
+    plt.xlabel('Valor de n')
+    plt.ylabel('Tempo de execução (ms)')
+    plt.show()
 
 
-ns = []
-tempos = []
-
-for c in range(1, 31):
-    inicio = time.perf_counter()
-    p1()
-    final = time.perf_counter()
-    ms = (final-inicio) * 10 ** 6
-    ns.append(c)
-    tempos.append(ms)
-
-plt.plot(ns, tempos)
-plt.xlabel('Valor de n')
-plt.ylabel('Tempo de execução (ms)')
-plt.show()
+while True:
+    esc = input('\n\n0 - Sair\n\n1 - Exercício\n2 - Gerar BigO\n- ')
+    if esc == '1':
+        p1()
+    elif esc == '2':
+        BigO()
+    elif esc == '0':
+        break
+    else:
+        print('Valor incorreto')
